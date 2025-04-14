66c2c5c1c4f59ac189350568608cd7058a397214
giragon6 <149984595+giragon6@users.noreply.github.com>
2025-04-14 18:45:36 -0500

    test: fix test bugs

--- src/routes/page.svelte.test.ts
+++ src/routes/page.svelte.test.ts
@@ -13,7 +13,7 @@ describe('/+page.svelte', () => {
 		render(Page);
 		const heading = screen.getByRole('heading', { level: 1 });
 		expect(heading).toHaveTextContent(
-			'Pioneering innovative digital solutions for global mental well-being.'
+			/Pioneering innovative digital solutions for global mental well-being./
 		);
 	});
 
@@ -25,7 +25,7 @@ describe('/+page.svelte', () => {
 
 	test('should render image with alt text', () => {
 		render(Page);
-		const image = screen.getByAltText("A stylized sketch of a girl's face.");
+		const image = screen.getByAltText(/A stylized sketch of a girl's face./);
 		expect(image).toBeInTheDocument();
 		expect(image.tagName).toBe('IMG');
 	});
@@ -39,20 +39,20 @@ describe('/+page.svelte', () => {
 		render(Page);
 		const heading = screen.getAllByRole('heading', { level: 2 })[0];
 		expect(heading).toHaveTextContent(
-			'mercuri.world makes mental health resources accessible to everyone.'
+			/mercuri.world makes mental health resources accessible to everyone./
 		);
 	});
 
 	test('should render p', () => {
 		render(Page);
-		expect(screen.getByRole('paragraph')).toBeInTheDocument();
+		expect(screen.getAllByRole('paragraph')[0]).toBeInTheDocument();
 	});
 
 	test('should render p with correct text', () => {
 		render(Page);
-		const heading = screen.getByRole('paragraph');
-		expect(heading).toHaveTextContent(
-			'We are a collective of activists, researchers, engineers, and creatives based in over a dozen nations. We develop, translate, and curate evidence-based digital mental health resources and disseminate them digitally and through our global outreach network.'
+		const paragraph = screen.getAllByRole('paragraph')[0];
+		expect(paragraph).toHaveTextContent(
+			/We are a collective of activists, researchers, engineers, and creatives based in over a dozen nations. We develop, translate, and curate evidence-based digital mental health resources and disseminate them digitally and through our global outreach network/
 		);
 	});
 });
