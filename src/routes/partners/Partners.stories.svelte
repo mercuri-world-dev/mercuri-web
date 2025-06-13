92e0a908c4dd5a12581116b93610eff68f85dfc6
giragon6 <emmemcdonald66@gmail.com>
2025-06-13 12:55:13 -0400

    fix: li rendering with app.css

--- src/routes/partners/Partners.stories.svelte
+++ src/routes/partners/Partners.stories.svelte
@@ -1,10 +1,10 @@
 <script module>
 	import { defineMeta } from '@storybook/addon-svelte-csf';
-	import Projects from './+page.svelte';
+	import Partners from './+page.svelte';
 
 	const { Story } = defineMeta({
 		title: 'Pages/Partners',
-		component: Projects,
+		component: Partners,
 		tags: ['autodocs'],
 		parameters: {
 			layout: 'fullscreen'
