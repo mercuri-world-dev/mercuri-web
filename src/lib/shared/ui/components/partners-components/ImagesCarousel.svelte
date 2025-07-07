d56b7ae954be42c791a5c067f54790d131478454
Simeonk1307 <112301031@smail.iitpkd.ac.in>
2025-07-08 00:27:38 +0530

    feat: copied ImagesCarousel component and changed it for using it in partners page easily

--- src/lib/shared/ui/components/partners-components/ImagesCarousel.svelte
+++ src/lib/shared/ui/components/partners-components/ImagesCarousel.svelte
@@ -2,14 +2,27 @@
     import { onMount } from 'svelte';
 
     export let images: { id: number; path: string; alt: string }[] = [];
-    export let speed = 15;
+    export let speed: number = 15;
+    export let left_to_right: boolean = true;
+
+    export let grayfilter: boolean = false;
+    export let logoStyle: boolean = false;
+
+
+    let array: Array<number> = logoStyle ? Array(4) : Array(2);
+    // export let 
     let animation: Animation | null=null;
     let trackElement: HTMLElement;
     onMount(() => {
         if (trackElement) {
-            const keyframes = [
+            const keyframes = left_to_right ? [
                 { transform: 'translateX(-50%)' },
-                { transform: 'translateX(0%)' }
+                { transform: 'translateX(0%)' },
+
+            ] : 
+            [
+                { transform: 'translateX(0%)' },
+                { transform: 'translateX(-50%)' }
             ];
 
             animation = trackElement?.animate?.(keyframes, {
@@ -20,11 +33,35 @@
     });
 </script>
 
-<style>
+<div data-testid="partners-carousel"
+     class="carousel-container" 
+     role="group" 
+     on:mouseenter={() => animation?.updatePlaybackRate?.(0.5)}
+     on:mouseleave={() => animation?.updatePlaybackRate?.(0.8)}>
+    <div class="carousel-track" bind:this={trackElement}>
+        {#each array as _}
+            {#each images as image (image.id)}
+                <div class="carousel-item" style="{logoStyle ? 'height:200px; width:230px;' : 'height:350px; width:450px;'}">
+                    <img 
+                        src={image.path} 
+                        alt={image.alt} 
+                        style="{ grayfilter ? '' : 'filter:none'}; { logoStyle ? 'background-color: var(--color-pale-purple); padding: 2rem' : '' };" 
+                        class=" rounded-4xl" 
+                        loading="lazy" />
+                </div>
+            {/each} 
+        {/each}   
+    </div>
+</div>
+
+
+
+<style lang="postcss">
+    @reference "tailwindcss";
+
     .carousel-container {
         overflow: hidden;
         position: relative;
-        padding: 2rem 0;
     }
 
     .carousel-container::before,
@@ -49,45 +86,22 @@
     }
 
     .carousel-track {
-        display: flex;
+        @apply flex flex-row gap-3;
         width: max-content;
     }
 
     .carousel-item {
-        flex: none;
-        width: 450px;
-        height: 350px;
-        margin: 0 0.5rem;
         transition: filter 0.3s ease;
     }
 
     .carousel-item img {
         width: 100%;
         height: 100%;
-        object-fit: cover;
         filter: grayscale(100%);
         transition: filter 0.3s ease;
     }
     .carousel-item:hover img {
         filter: grayscale(0%);
     }
+    
 </style>
-
-<div data-testid="partners-carousel"
-     class="carousel-container" 
-     role="group" 
-     on:mouseenter={() => animation?.updatePlaybackRate?.(0.5)}
-     on:mouseleave={() => animation?.updatePlaybackRate?.(1)}>
-    <div class="carousel-track" bind:this={trackElement}>
-        {#each images as image (image.id)}
-            <div class="carousel-item">
-                <img src={image.path} alt={image.alt} class=" rounded-4xl" loading="lazy" />
-            </div>
-        {/each}
-        {#each images as image (image.id)} 
-            <div class="carousel-item">
-                <img src={image.path} alt={image.alt} class=" rounded-4xl" loading="lazy" />
-            </div>
-        {/each}
-    </div>
-</div>
