0327f0ee882fbf242da23468c62ed4e319e58cb6
Sophia Groth <78243082+soph1428@users.noreply.github.com>
2025-08-14 07:08:14 -0500

    Feat: added and tested sponsor page (#17)
    * chore: add sponsor stub
    
    * test: add tests
    
    * feat: added and testing sponsor page requirements
    
    ---------
    
    Co-authored-by: giragon6 <emmemcdonald66@gmail.com>

---
+++ src/routes/sponsor/Sponsor.stories.svelte
@@ -0,0 +1,15 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Sponsor from './+page.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Pages/Sponsor',
+		component: Sponsor,
+		tags: ['autodocs'],
+		parameters: {
+			layout: 'fullscreen'
+		}
+	});
+</script>
+
+<Story name="Default" />
